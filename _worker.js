4dad4a11888ca447bb24cef034e588e321bcedc1
ziyi233 <1790060255@qq.com>
2025-08-21 21:54:24 +0800

    fix: Use CommonJS in _worker.js

--- _worker.js
+++ _worker.js
@@ -1,10 +1,11 @@
 // _worker.js
 // This file is the entry point for Cloudflare Pages.
 
-// Import the Express app instance from our main application file.
+// Import the Express app instance from our main application file using CommonJS.
 const app = require('./index.js');
 
 // The Cloudflare Pages environment expects a default export with a `fetch` method.
-export default {
+// We provide the Express app instance directly, which Cloudflare can adapt.
+module.exports = {
   fetch: app,
 };
